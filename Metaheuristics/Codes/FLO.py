6bf43965aed16511b57f1063c142da7706299861
Shaseram <vicente.arratia.c@mail.pucv.cl>
2024-10-14 23:20:22 -0300

    Correcion FLO, distribucion normal asignada correctamente

--- Metaheuristics/Codes/FLO.py
+++ Metaheuristics/Codes/FLO.py
@@ -13,30 +13,30 @@ def iterarFLO(maxIter, iter, dim, population, fitness, bestSolution, fo, typePro
             prey = population[np.random.randint(population.__len__())]
         
         I = np.random.choice([1, 2])
-        r = np.random.rand(dim)
+        r = np.random.normal(0,1)
         
         # Nueva posición basada en la presa (fase de exploración)
         newPositionFL = population[i] + r * (prey - I * population[i])
         newPositionFL = np.clip(newPositionFL, lowerBound, upperBound)
 
         # Evaluar la nueva posición
-        repaired_position, newFitnessFL = fo(newPositionFL)
+        newPositionFL, newFitnessFL = fo(newPositionFL)
 
         # Actualizar si la nueva posición es mejor
         if (typeProblem == 'MIN' and fitness[i] > newFitnessFL) or (typeProblem == 'MAX' and fitness[i] < newFitnessFL):
             population[i] = newPositionFL
             fitness[i] = newFitnessFL
 
-    # Fase 2: Explotación (Moving up the tree)
+        # Fase 2: Explotación (Moving up the tree)
     for i in range(population.__len__()):
-        r2 = np.random.rand(dim)
+        r2 = np.random.normal(0,1)
         
         # Nueva posición para la fase de explotación (subida al árbol)
         newPositionTree = population[i] + (1 - r2) * ((upperBound - lowerBound) / (iter + 1))
         newPositionTree = np.clip(newPositionTree, lowerBound, upperBound)
 
         # Evaluar la nueva posición en la fase de explotación
-        repaired_position, newFitnessTree = fo(newPositionTree)
+        newPositionFL, newFitnessTree = fo(newPositionTree)
 
         # Actualizar si la nueva posición es mejor
         if (typeProblem == 'MIN' and fitness[i] > newFitnessTree) or (typeProblem == 'MAX' and fitness[i] < newFitnessTree):
